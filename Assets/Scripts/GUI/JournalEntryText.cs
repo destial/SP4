31adc2c799647ea24d6a3f4b61a57fdba5348447
Draco-Matrix <SiawYuYingDouglas@gmail.com>
2022-02-25 12:03:30 +0800

    Journal Entry Stuff
    Journal Entry stuff works now. Currently only added one. This includes:
     - Journal Entry GameObject
     - Journal Entry UI
     - Clipboard Asset Package for the Journal Entries.

---
+++ Assets/Scripts/GUI/JournalEntryText.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalEntryText : MonoBehaviour
+{
+    [SerializeField] public string journalEntryNo;
+    
+    [HideInInspector] public bool onTrigger;
+    [HideInInspector] public bool textScreen;
+
+    public GameObject journalEntryUI;
+
+    void OnTriggerEnter(Collider other)
+    {
+        onTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        onTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        if (onTrigger)
+        {
+            // Rect rect = PlayerManager.instance.GetComponentInChildren<Camera>().
+            GUI.Box(new Rect(350, 250, 300, 25), "Press 'F' to open Journal Entry #" + journalEntryNo);
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                textScreen = true;
+                onTrigger = false;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                PlayerMovement.instance.canMove = false;
+                GameStateManager.Instance.SetState(GameState.Keypad);
+            }
+        }
+
+        if (textScreen)
+        {
+            JournalEntries();
+        }
+    }
+
+    public void JournalEntries()
+    {
+        journalEntryUI.SetActive(true);
+        GameStateManager.Instance.SetState(GameState.Keypad);
+        Cursor.visible = true;
+        PlayerMovement.instance.canMove = false;
+    }
+
+    public void QuitText()
+    {
+        textScreen = false;
+        GameStateManager.Instance.SetState(GameState.Gameplay);
+        Cursor.visible = false;
+        PlayerMovement.instance.canMove = true;
+    }
+}
