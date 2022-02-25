a25e6109a368c671d3ba4540f10f3baecb9762df
Mark Albert Carreon <mark.albert1995@gmail.com>
2022-02-25 14:05:03 +0800

    Stuff
    Marscene3

--- Assets/Scripts/Enemy/FSM/Chase.cs
+++ Assets/Scripts/Enemy/FSM/Chase.cs
@@ -7,9 +7,15 @@ public class Chase : BaseState
 {
     private Zombie _zombie;
 
+    private readonly LayerMask _layerMask = LayerMask.NameToLayer("Walls");
+    private float _rayDistance = 3.5f;
+    private Quaternion _desiredRotation;
+
     //Animation Vars
     const string RUN = "Zombie_Run";
     const string ATTACK = "Zombie_Attack";
+
+
 
 
     public Chase(Zombie zombie) : base(zombie.gameObject)
@@ -18,33 +24,51 @@ public class Chase : BaseState
         animationManager = _zombie.GetComponent<AnimationManager>();
     }
 
+    private bool isForwardBlocked()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        return Physics.SphereCast(ray, 0.5f, _rayDistance, _layerMask);
+    }
+
     public override Type Tick()
     {
         if (_zombie.Target == null) return typeof(Patrol);
+
+
         //Zombie's Target
         Vector3 targetPos = new Vector3(_zombie.Target.position.x,
                                         transform.position.y,
                                         _zombie.Target.position.z);
-        //transform.LookAt(_zombie.Target);
-        transform.LookAt(targetPos);
-        transform.Translate(Vector3.forward * Time.deltaTime * GameSettings.Instance.zombieSpeed * 5);
+
+        transform.LookAt(_zombie.Target);
+        
+        //transform.LookAt(targetPos);
+        transform.Translate(transform.forward * Time.deltaTime * GameSettings.Instance.zombieSpeed * 2);
 
         //Distance between ENEMY & PLAYER
         var distance = Vector3.Distance(transform.position, targetPos);
 
-        // Debug.Log("CHASING PLAYER");
-        //transform.Translate(Vector3.forward * Time.deltaTime * GameSettings.Instance.zombieSpeed * 5);
-        //ATTACK, When enemy reached player within distance
-        if(distance <= GameSettings.Instance.attackRange)
+        if(distance <= GameSettings.Instance.aggroRadius)
         {
             animationManager.ChangeAnimationState(ATTACK);
             Debug.Log("ATTACKED PLAYER");
         }
-        else if(distance <= 40f) //GameSettings.Instance.aggroRadius)
+        else if(distance <= 20f)
         {
-            Debug.Log("CHASING TARGET");
-            transform.Translate(transform.forward.normalized * Time.deltaTime * GameSettings.Instance.zombieSpeed * 5);
-            animationManager.ChangeAnimationState(RUN);
+            if (isForwardBlocked())
+            {
+                Debug.Log("FORWARD BLOCKED TRUE! CHASE");
+                transform.rotation = Quaternion.Lerp(transform.rotation, _desiredRotation, 0.2f);
+            }
+            else
+            {
+                Debug.Log("FORWARD BLOCKED FALSE! CHASE");
+                //transform.Translate(Vector3.forward * Time.deltaTime * GameSettings.Instance.zombieSpeed * 5);
+
+                Debug.Log("CHASING TARGET");
+                transform.Translate(transform.forward * Time.deltaTime * GameSettings.Instance.zombieSpeed * 5);
+                animationManager.ChangeAnimationState(RUN);
+            }
         }
         //Player is out of range in enemy vision
         else
@@ -52,6 +76,12 @@ public class Chase : BaseState
             //transform.LookAt(targetPos);
             return typeof(Patrol);
         }
+
+
+
+
         return null;
     }
 }
+
+
